e004e3a6257fd28c3bd256e92f293f106bb71b96
oshankiii12 <oshankipriya1@gmail.com>
2025-12-08 00:07:05 +0530

    feat: fixed images data rendering

---
+++ backend/app/data/download_car_images.py
@@ -0,0 +1,31 @@
+import gdown, os
+import zipfile
+from pathlib import Path
+
+FILE_ID = os.getenv("CARS_ZIP_ID")
+
+BASE_DIR = Path(__file__).resolve().parents[2]
+DOWNLOAD_DIR = BASE_DIR / "static" 
+DOWNLOAD_DIR.mkdir(parents=True, exist_ok=True)
+
+ZIP_PATH = BASE_DIR / "app" / "data" / "cars_images.zip"
+
+def main():
+    print("Downloading ZIP from Google Drive...")
+    gdown.download(
+        id=FILE_ID,
+        output=str(ZIP_PATH),
+        quiet=False
+    )
+
+    print("Download complete")
+    print("Unzipping...")
+
+    with zipfile.ZipFile(ZIP_PATH, "r") as z:
+        z.extractall(DOWNLOAD_DIR)
+
+    print("Images extracted to:", DOWNLOAD_DIR)
+    ZIP_PATH.unlink() 
+
+if __name__ == "__main__":
+    main()
